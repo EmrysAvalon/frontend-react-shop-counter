b7686c8c725210923bffcf07a151ee0072c09d5d
EmrysAvalon <tanja@hunen.nl>
2020-11-17 17:00:13 +0100

    done assignment

--- src/App.js
+++ src/App.js
@@ -1,11 +1,38 @@
-import React from 'react';
+import React, {Fragment, useState} from 'react';
 import './App.css';
 
 function App() {
+
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        console.log(`Incremented ${count} by 1`);
+        setCount(count + 1);
+    }
+
+    const reduce = () => {
+        console.log(`Reduced ${count} by 1`);
+        setCount(count - 1)
+    }
+
+    const reset = () => {
+        console.log(`Reset ${count} to 0`);
+        setCount(0);
+    }
+
   return (
-    <div>
-      Begin hier met het bouwen van de applicatie!
-    </div>
+      <Fragment>
+          <div className="current_value">
+              <h2 className="value">There are {count} people in the store.</h2>
+          </div>
+          <div className="counters">
+              <button type="button" onClick={() => reduce()}>-</button>
+              <button type="button" onClick={() => increment()}>+</button>
+          </div>
+          <div className="reset">
+              <button type="button" onClick={() => reset()}>Reset</button>
+          </div>
+      </Fragment>
   );
 }
 
